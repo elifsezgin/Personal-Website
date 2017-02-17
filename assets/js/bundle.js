1c4838f41d397f06fb4f27921321428a43824c47
Elif Sezgin <elifmsezgin@gmail.com>
2017-02-16 23:00:26 -0800

    Added email feature

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -169,57 +169,63 @@ var $name = $('input[name="name"]');
 var $email = $('input[name="email"]');
 var $subject = $('input[name="subject"]');
 var $message = $('input[name="message"]');
-var $submit = $('#submit');
+var $form = $('form#email-form');
 
-var data = {
-	'name': null,
-	'email': null,
-	'subject': null,
-	'message': null
-};
+// let data = {
+// 	'name': null,
+// 	'email': null,
+// 	'subject': null,
+// 	'message': null
+// };
 
-function update(field, value) {
-	data[field] = value;
-}
-console.log($name);
+// function update(field, value) {
+// 	data[field] = value;
+// }
+// console.log($name);
 // debugger;
-$name.on('input', function (e) {
-	update('name', e.target.value);
-});
-$email.on('input', function (e) {
-	update('email', e.target.value);
-});
-$subject.on('input', function (e) {
-	update('subject', e.target.value);
-});
-$message.on('input', function (e) {
-	update('message', e.target.value);
-});
+// $name.on('input', (e) => {
+// 	update('name', e.target.value);
+// });
+// $email.on('input', (e) => {
+// 	update('email', e.target.value);
+// });
+// $subject.on('input', (e) => {
+// 	update('subject', e.target.value);
+// });
+// $message.on('input', (e) => {
+// 	update('message', e.target.value);
+// });
 
-$submit.on('click', function (e) {
-	debugger;
-	emailjs.send("gmail", "template_Roq16NhR", {
-		name: "Elif",
-		notes: "Check this out!"
-	});
-});
+// $submit.on('click', (e) => {
+// 	debugger;
+// 	emailjs.send("gmail","template_Roq16NhR",{
+//   name: "Elif",
+//   notes: "Check this out!"
+// 	});
+// });
 
-function sendMail(e) {
-	debugger;
-	$.ajax({
-		type: 'POST',
-		url: 'https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0@api.mailgun.net/v3/samples.mailgun.org/messages',
-		headers: { "Access-Control-Allow-Headers": "Authorization" },
-		data: {
-			"from": "Mailgun Sandbox <postmaster@sandbox213b4c5914174bf2bc82132b3f377be8.mailgun.org>",
-			"to": "Elif <elifsezgin1234@gmail.com>",
-			"subject": "Hello Elif",
-			"text": "Congratulations Elif, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
-		}
-	}).done(function (response) {
-		console.log(response); // if you're into that sorta thing
+$form.submit(function (event) {
+	event.preventDefault();
+
+	var params = $form.serializeArray().reduce(function (obj, item) {
+		console.log(obj);
+		obj[item.name] = item.value;
+		return obj;
+	}, {});
+
+	// Change to your service ID, or keep using the default service
+	var service_id = "gmail";
+
+	var template_id = "template_Roq16NhR";
+	$form.find("button").text("Sending...");
+	emailjs.send(service_id, template_id, params).then(function () {
+		alert("Sent!");
+	}, function (err) {
+		alert("Send email failed!\r\n Please send your email to elifmsezgin@gmail.com");
+		$form.find("button").text("Send");
 	});
-}
+	return false;
+});
 
 /***/ })
 /******/ ]);
